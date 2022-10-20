e3bc1cd00bf56c64c6ce7b3d711775c7d2487d45
MathanSakthivel07 <116252022+MathanSakthivel07@users.noreply.github.com>
2022-10-20 13:32:33 +0530

    Update file3.js

--- file3.js
+++ file3.js
@@ -1,45 +1,11 @@
-describe("master-service.js", () => {
-  it("Test 1", () => {
-    // Arrange
-    const http = "fakeHttpMock";
-    const logger = "fakeLoggerMock";
-    const anotherParameter = "fakeAnotherParameterMock";
-    const masterService = new MasterService({
-      http,
-      logger,
-      anotherParameter,
-    });
-
-    // Act & Assert..
-  });
-
-  it("Test 2", () => {
-    // Arrange
-    const http = "fakeHttpMock";
-    const logger = "fakeLoggerMock";
-    const anotherParameter = "fakeAnotherParameterMock";
-    const masterService = new MasterService({
-      http,
-      logger,
-      anotherParameter,
-    });
-
-    // Act & Assert..
-  });
-
-  it("Test 3", () => {
-    // Arrange
-    const http = "--anotherFakeHttpMock--";
-    const logger = "fakeLoggerMock";
-    const anotherParameter = "fakeAnotherParameterMock";
-    const masterService = new MasterService({
-      http,
-      logger,
-      anotherParameter,
-    });
-
-    // Act & Assert..
-  });
-
-  // Other 17 tests...
-});
+window.onload = function() 
+{	var btn = document.getElementById('btn'); 
+	btn.addEventListener('click', b, false); 
+	btn.addEventListener('click', c, false); 
+} 
+function b() 
+{	/* some code here */ 
+} 
+function c() 
+{	/* some other code here */ 
+} 
