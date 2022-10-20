9befa20cc576b6e12e4f0a029a0b0c56d4e2d2f5
MathanSakthivel07 <116252022+MathanSakthivel07@users.noreply.github.com>
2022-10-20 11:46:44 +0530

    Create sam.js

---
+++ sam.js
@@ -0,0 +1,28 @@
+function MyFunc2(a) {
+  if (a) {
+    if (a) {
+      if (a) {
+        if (a) {
+          if (a) {
+            if (a) {
+              if (a) {
+                if (a) {
+                  if (a) {
+                    if (a) {
+                      if (a) {
+                        console.log(a);
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+}
+
+MyFunc2();
