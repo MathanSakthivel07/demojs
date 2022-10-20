7029ea2ffbe671b3797b204d0c7134c3bf8376ed
MathanSakthivel07 <116252022+MathanSakthivel07@users.noreply.github.com>
2022-10-20 13:05:27 +0530

    Update sam.js

--- sam.js
+++ sam.js
@@ -25,32 +25,4 @@ function MyFunc2(a) {
   }
 }
 
-public Position WalkNorth()
-{
-   var player = GetPlayer();
-   player.Move("N");
-   return player.NewPosition;
-}
- 
-public Position WalkSouth()
-{
-   var player = GetPlayer();
-   player.Move("S");
-   return player.NewPosition;
-}
- 
-public Position WalkEast()
-{
-   var player = GetPlayer();
-   player.Move("E");
-   return player.NewPosition;
-}
- 
-public Position WalkWest()
-{
-   var player = GetPlayer();
-   player.Move("W");
-   return player.NewPosition;
-}
-
 MyFunc2(1);
