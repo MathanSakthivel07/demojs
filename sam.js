a175c80933b42ca22005b08367d1dc4e7b117086
MathanSakthivel07 <116252022+MathanSakthivel07@users.noreply.github.com>
2022-10-20 12:59:41 +0530

    Update sam.js

--- sam.js
+++ sam.js
@@ -25,31 +25,4 @@ function MyFunc2(a) {
   }
 }
 
-function MyFunc2(a) {
-  if (a) {
-    if (a) {
-      if (a) {
-        if (a) {
-          if (a) {
-            if (a) {
-              if (a) {
-                if (a) {
-                  if (a) {
-                    if (a) {
-                      if (a) {
-                        console.log(a);
-                        console.log(a);
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
 MyFunc2(1);
